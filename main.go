f4076c5b20619a81203793d6172a864483b80521
logeshwhatever <beingabumpkin@gmail.com>
2025-05-27 12:08:29 +0000

    added page_report function

--- main.go
+++ main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 4 {
 		fmt.Println("no website provided")
 		return
 	}
-	if len(os.Args) > 2 {
+	if len(os.Args) > 4 {
 		fmt.Println("too many arguments provided")
 		return
 	}
 	rawBaseURL := os.Args[1]
+	maxConcurrencyString := os.Args[2]
+	maxPagesString := os.Args[3]
 
-	const maxConcurrency = 3
-	cfg, err := configure(rawBaseURL, maxConcurrency)
+
+	maxConcurrency, err := strconv.Atoi(maxConcurrencyString)
+	if err != nil {
+		fmt.Printf("Error - maxConcurrency: %v", err)
+		return
+	}
+
+	maxPages, err := strconv.Atoi(maxPagesString)
+	if err != nil {
+		fmt.Printf("Error - maxPages: %v", err)
+		return
+	}
+
+	cfg, err := configure(rawBaseURL, maxConcurrency, maxPages)
 	if err != nil {
 		fmt.Printf("Error - configure: %v", err)
 		return
@@ -29,8 +44,5 @@ func main() {
 	go cfg.crawlPage(rawBaseURL)
 	cfg.wg.Wait()
 
-	for normalizedURL, count := range cfg.pages {
-		fmt.Printf("%d - %s\n", count, normalizedURL)
-	}
+	printReport(cfg.pages, rawBaseURL)
 }
-
